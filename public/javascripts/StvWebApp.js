c2429afb0f39c8152294ca169f904a7eedc2c46d
Gobackhome <minhtran.dn.it@gmail.com>
2016-07-21 11:48:07 +0700

    index view

--- public/javascripts/StvWebApp.js
+++ public/javascripts/StvWebApp.js
@@ -1,16 +1,16 @@
  var app = angular.module('StvWebApp', []);
- app.factory('userArea', [function() {
+ app.factory('indexArea', [function() {
      var o = {
-         posts: [],
+         newposts: [],
+         viewPosts: [],
+         featurePost: [],
          categories: []
      };
      return o;
      
  }]);
 
- app.controller('UserAreaCtrl', ['$scope', 'userArea', '$http', '$window', function($scope, userArea, $http, $window) {
-     var path = '/public/partialviews/userArea/';
-     $scope.userAreaContent = path + 'about.html';
+ app.controller('Index', ['$scope', 'indexArea', '$http', '$window', function($scope, userArea, $http, $window) {
 
      $scope.onLoad = function() {
          $http.get('/post')
@@ -25,68 +25,6 @@
          $scope.categories = userArea.categories;
          console.log(userArea.categories);
      }
-     $scope.setUserAreaContent = function(filename) {
-         $scope.userAreaContent = path + filename;
-     };
-     $scope.addPost = function(post) {
-         //Luu vao factory
-         $scope.posts.push(post);
-         //Goi Http luu vao database
-         $http.post('/posts', post)
-             .success(function(data, status, headers, config) {
-                 $window.alert(data.msg);
-             })
-             .error(function(data, status, headers, config) {
-                 $window.alert(data.msg);
-             })
-     };
-     $scope.addCategory = function(category) {
-         $scope.categories.push(category);
-         $http.post('/categories', category)
-             .success(function(data, status, headers, config) {
-                 $window.alert(data.msg);
-             })
-             .error(function(data, status, headers, config) {
-                 $window.alert(data.msg);
-             })
-     };
 
  }]);
- app.controller('AuthCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
-     var pathAuth = '/public/partialviews/user/';
-     $scope.content = pathAuth + 'login.html';
-     $scope.setContent = function(filename) {
-         $scope.content = pathAuth + filename;
-     };
-
-     $scope.doSignup = function() {
-         $http.post('/user', {
-                 displayName: $scope.displayName,
-                 email: $scope.email,
-                 password: $scope.password
-             })
-             .success(function(data, status, headers, config) {
-                 $scope.setContent(pathAuth + 'login.html');
-             })
-             .error(function(data, status, headers, config) {
-                 $window.alert(data.msg);
-             })
-     };
- }]);
- //
- //app.controller('FileController', function($scope, $resource) {
- //
- //    var Files = $resource('/files/:id', { id: "@id" });
- //
- //    angular.extend($scope, {
- //
- //        post: { file: null },
- //
- //        upload: function(post) {
- //            Files.prototype.$save.call(post.file, function(self, headers) {
- //                // Handle server response
- //                console.log(post.file);
- //            });
- //        }
- //    });
- //});
+ 
